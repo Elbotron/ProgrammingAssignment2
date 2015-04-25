6024824fd2683ba2dc18a49d0b9771d2c4cd34d2
Elbotron <elbercarneiro@gmail.com>
2015-04-25 17:44:26 -0400

    Finished assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## creates a cache for our solved variable (result of the solve() function).
+## if the result of solve(x) has already been computed, we will use this 
+## function to retrieve the saved result instead of computing solve(x) again,
+## which could be a computationally expensive task.
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## set solved to null initially
+  solved <- NULL
+  
+  ## sets the value of our matrix x and sets solved to null
+  set <- function(y) {
+    x <<- y
+    solved <<- NULL
+  }
+  
+  ## this function returns our matrix x
+  get <- function() x
+  
+  ## sets solved to the argument s
+  setSolved <- function(s) solved <<- s
+  
+  ## returns solved
+  getSolved <- function() solved
+  
+  list(set = set, get = get, setSolved = setSolved, 
+       getSolved = getSolved)
 
 }
 
 
-## Write a short comment describing this function
-
+## This function will first check to see if solve(x) was computed. If not, it
+## will compute solve(x) and return it. If it has already been computed and
+## cached, it will return the cached value.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## get cached value
+  solved <- x$getSolved()
+  
+  ## if cached value is not null...
+  if(!is.null(solved)) {
+    
+    message("Getting cached data...")
+    
+    ## ...return cached value
+    return(solved)
+  }
+  
+  ## othewise:
+  
+  ## get the value of the matrix x
+  data <- x$get()
+  
+  ## compute solve(data)
+  solved <- solve(data, ...)
+  
+  ## save the computed value in the cache for future use
+  x$setSolved(solved)
+  
+  ## return the computed value
+  solved
+  
 }
